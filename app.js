c400bc5222270ca4e2dbacaa1c38eaf3c991bc5f
realNasirMalik <realNasirMalik@gmail.com>
2019-06-24 20:53:30 -0500

    :bento: Implement signup login and logout routes
    Implemented signup login and logout routes with Passport.js
    
    modified:   app.js
    modified:   controllers/landing.js
    modified:   controllers/user.js
    modified:   passport_setup.js
    modified:   routes/index.js
    modified:   routes/users.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -22,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret: 'our new secret'}));
+app.use(session({ secret: 'our new secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -31,18 +32,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// render the error page
+res.status(err.status || 500);
+res.render('error');
 });
 
 module.exports = app;
